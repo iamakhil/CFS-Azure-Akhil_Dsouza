9937abf639c99979f5660ec6a5467f4d6d2559eb
iamakhil <31007628+iamakhil@users.noreply.github.com>
2021-07-19 21:02:52 +0530

    Create variable.tf

---
+++ terraform code/variable.tf
@@ -0,0 +1,15 @@
+variable "rgname" {
+  default = "rg-sea-nh"
+}
+
+variable "region" {
+  default = "southeastasia"
+}
+
+variable "rgname2" {
+  default = "rg-eus-nh"
+}
+
+variable "region2" {
+  default = "eastus"
+}
